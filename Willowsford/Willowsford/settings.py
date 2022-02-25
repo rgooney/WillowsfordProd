483484f60f894f1270e37995e1c69f3f88f0d797
David Miles <dwmdominion8@gmail.com>
2022-02-24 19:27:39 -0500

    Cleaned up settings.py

--- Willowsford/Willowsford/settings.py
+++ Willowsford/Willowsford/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ken998r(*k1!g0rqv*x5grxsvm2cq3xg*lc4zingc0t4z)a-z#'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
