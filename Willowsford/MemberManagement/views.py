199672696d9cb3967cfa748263f9412781dce87d
David <dwmdominion8@gmail.com>
2022-04-02 17:47:49 -0400

    Log in added. Needs indication of login status.

--- Willowsford/MemberManagement/views.py
+++ Willowsford/MemberManagement/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def signIn(request):
-    return render(request, 'MemberManagement/signIn.html')
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                messages.error(request, "Invalid username or password")
+        else:
+            messages.error(request, "Invalid username or password")
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'MemberManagement/signIn.html', {"form": form})
