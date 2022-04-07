a59e71911698dcdb62f95ff19247b3285c1f3bbb
dmiles4 <dwmdominion8@gmail.com>
2022-04-07 17:41:44 -0400

    Cleaned up directories and added form

--- Willowsford/Scoring/urls.py
+++ Willowsford/Scoring/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from Willowsford.Scoring import views
+from . import views
 from django.urls import re_path
 #from django.conf.urls import url
 
 urlpatterns = [
-    path('', views.root, name ='root'),
-    path('', views.index, name='index'),
+    path('manualScoring', views.manualScoring, name="manualScoring"),
     path('calculateScore', views.calculateScore, name="calculateScore"),
     path('receiveInput', views.receiveInput, name="receiveInput"),
 ]
--- Willowsford/Willowsford/settings.py
+++ Willowsford/Willowsford/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'HomeSite.apps.HomesiteConfig',
     'MemberManagement.apps.MembermanagementConfig',
     'Registration.apps.RegistrationConfig',
-    'Scoring.apps.ScoringConfig'
+    'Scoring.apps.ScoringappConfig'
 ]
 
 MIDDLEWARE = [
