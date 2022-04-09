8bcccb1c4d3441a9d7251a5e86338ab364752284
rgooney <rachel9903@gmail.com>
2022-04-08 22:04:21 -0400

    reverting

--- Willowsford/Willowsford/settings.py
+++ Willowsford/Willowsford/settings.py
@@ -126,20 +126,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# This should be at the start of the file
-import environ
-
-env = environ.Env()
-environ.Env.read_env()
-
-# Previous settings ...
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = env('EMAIL_HOST')
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-
-# Custom setting. To email
-RECIPIENT_ADDRESS = env('RECIPIENT_ADDRESS')
