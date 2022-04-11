008998fdb5cdc8f3c8b139e76839d05a73faa9f5
David <dwmdominion8@gmail.com>
2022-04-10 21:57:53 -0400

    Initial setup for new form

--- Willowsford/Registration/views.py
+++ Willowsford/Registration/views.py
@@ -3,31 +3,35 @@ from django.urls import reverse
 from .forms import *
 
 # Create your views here.
-def registerpone(request):
+def register(request):
     if request.POST:
-        form = UserAccountForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
+        user_form = UserAccountForm(request.POST)
+        extended_user_form = RegistrationForm(request.POST)
+        if user_form.is_valid() and extended_user_form.is_valid():
+            user = user_form.save(commit=False)
             user.save()
-            return HttpResponseRedirect(reverse('registerptwo', args=[user.username]))
-    else:
-        form = UserAccountForm()
-
-    return render(request, 'Registration/registration.html', {'form': form})
-
-def registerptwo(request, user_fk):
-    user_account = User.objects.get(username=user_fk)
-    if request.POST:
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.username = user_account
-            user.save()
+            extended_user_info = extended_user_form.save(commit=False)
+            extended_user_info.save()
             return HttpResponseRedirect(reverse('index'))
-        # else:
-        #     return render(request, 'registration/registration.html', {'form': form})
     else:
-        form = RegistrationForm()
+        user_form = UserAccountForm()
+        extended_user_form = RegistrationForm()
 
-    return render(request, 'Registration/registration2.html', {'form': form, 'username': user_fk})
+    return render(request, 'Registration/registration.html', {'user_form': user_form, 'extended_user_form': extended_user_form})
+
+# def registerptwo(request, user_fk):
+#     user_account = User.objects.get(username=user_fk)
+#     if request.POST:
+#         form = RegistrationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save(commit=False)
+#             user.username = user_account
+#             user.save()
+#             return HttpResponseRedirect(reverse('index'))
+#         # else:
+#         #     return render(request, 'registration/registration.html', {'form': form})
+#     else:
+#         form = RegistrationForm()
+#
+#     return render(request, 'Registration/registration2.html', {'form': form, 'username': user_fk})
 
