324b9892bfa77da79fa061638dc9c79676ce4bb3
David <dwmdominion8@gmail.com>
2022-04-13 00:03:33 -0400

    Scoring integrated into database. Front end needs to be fixed up

--- Willowsford/Scoring/forms.py
+++ Willowsford/Scoring/forms.py
@@ -1,6 +1,6 @@
 from xml.etree.ElementTree import Comment
 from django import forms
-
+from .models import Scores
 
 class ManualScoringForm(forms.Form):
     #round 1
@@ -8,69 +8,91 @@ class ManualScoringForm(forms.Form):
     r1n2 = forms.IntegerField(initial=0)
     r1n3 = forms.IntegerField(initial=0)
     commentOne = forms.CharField(required = False)
-    cum1 = 0
-    sub1 = 0
+    cumulative1 = forms.IntegerField(initial=0)
+    subtotal1 = forms.IntegerField(initial=0)
     #round 2
     r2n1 = forms.IntegerField(initial=0)
     r2n2 = forms.IntegerField(initial=0)
     r2n3 = forms.IntegerField(initial=0)
     commentTwo = forms.CharField(required = False)
-    cum2 = 0
-    sub2 = 0
+    cumulative2 = forms.IntegerField(initial=0)
+    subtotal2 = forms.IntegerField(initial=0)
     #round 3
     r3n1 = forms.IntegerField(initial=0)
     r3n2 = forms.IntegerField(initial=0)
     r3n3 = forms.IntegerField(initial=0)
     commentThree = forms.CharField(required = False)
-    cum3 = 0
+    cumulative3 = 0
     sub3 = 0
     #round 4
     r4n1 = forms.IntegerField(initial=0)
     r4n2 = forms.IntegerField(initial=0)
     r4n3 = forms.IntegerField(initial=0)
     commentFour = forms.CharField(required = False)
-    cum4 = 0
+    cumulative4 = 0
     sub4 = 0
     #round 5
     r5n1 = forms.IntegerField(initial=0)
     r5n2 = forms.IntegerField(initial=0)
     r5n3 = forms.IntegerField(initial=0)
     commentFive = forms.CharField(required = False)
-    cum5 = 0
+    cumulative5 = 0
     sub5 = 0
     #round 6
     r6n1 = forms.IntegerField(initial=0)
     r6n2 = forms.IntegerField(initial=0)
     r6n3 = forms.IntegerField(initial=0)
     commentSix = forms.CharField(required = False)
-    cum6 = 0
+    cumulative6 = 0
     sub6 = 0
     #round 7
     r7n1 = forms.IntegerField(initial=0)
     r7n2 = forms.IntegerField(initial=0)
     r7n3 = forms.IntegerField(initial=0)
     commentSeven = forms.CharField(required = False)
-    cum7 = 0 
+    cumulative7 = 0
     sub7 = 0
     #round 8
     r8n1 = forms.IntegerField(initial=0)
     r8n2 = forms.IntegerField(initial=0)
     r8n3 = forms.IntegerField(initial=0)
     commentEight = forms.CharField(required = False)
-    cum8 = 0 
+    cumulative8 = 0
     sub8 = 0
     #round 9 
     r9n1 = forms.IntegerField(initial=0)
     r9n2 = forms.IntegerField(initial=0)
     r9n3 = forms.IntegerField(initial=0)
     commentNine = forms.CharField(required = False)
-    cum9 = 0 
+    cumulative9 = 0
     sub9 = 0
     #round 10
     r10n1 = forms.IntegerField(initial=0)
     r10n2 = forms.IntegerField(initial=0)
     r10n3 = forms.IntegerField(initial=0)
     commentTen = forms.CharField(required = False)
-    cum10 = 0
+    cumulative10 = 0
     sub10 = 0
     result = 0
+
+class ManualScoringModelForm(forms.ModelForm):
+    class Meta:
+        model = Scores
+        fields = (
+            "shooting_style", "distance", "score", "datetime",
+            "r1n1", "r1n2", "r1n3", "commentRoundOne", "cumulativeRoundOne", "subtotalRoundOne",
+            "r2n1", "r2n2", "r2n3", "commentRoundTwo", "cumulativeRoundTwo", "subtotalRoundTwo",
+            "r3n1", "r3n2", "r3n3", "commentRoundThree", "cumulativeRoundThree", "subtotalRoundThree",
+            "r4n1", "r4n2", "r4n3", "commentRoundFour", "cumulativeRoundFour", "subtotalRoundFour",
+            "r5n1", "r5n2", "r5n3", "commentRoundFive", "cumulativeRoundFive", "subtotalRoundFive",
+            "r6n1", "r6n2", "r6n3", "commentRoundSix", "cumulativeRoundSix", "subtotalRoundSix",
+            "r7n1", "r7n2", "r7n3", "commentRoundSeven", "cumulativeRoundSeven", "subtotalRoundSeven",
+            "r8n1", "r8n2", "r8n3", "commentRoundEight", "cumulativeRoundEight", "subtotalRoundEight",
+            "r9n1", "r9n2", "r9n3", "commentRoundNine", "cumulativeRoundNine", "subtotalRoundNine",
+            "r10n1", "r10n2", "r10n3", "commentRoundTen", "cumulativeRoundTen", "subtotalRoundTen",
+        )
+
+        widgets = {
+            "shooting_style": forms.RadioSelect(),
+            "datetime": forms.DateTimeInput(attrs={'type': 'datetime-local'})
+        }
--- Willowsford/Scoring/views.py
+++ Willowsford/Scoring/views.py
@@ -1,63 +1,62 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.models import User
 
-from .forms import ManualScoringForm
+from .forms import ManualScoringModelForm
 
 # Create your views here.
 
 def manualScoring(request):
     if request.POST:
-        form = ManualScoringForm(request.POST)
-        round1Sub = 0
-        round2Sub = 0
-        round3Sub = 0
-        round4Sub = 0
-        round5Sub = 0
-        round6Sub = 0
-        round7Sub = 0
-        round8Sub = 0
-        round9Sub = 0
-        round10Sub = 0
+        form = ManualScoringModelForm(request.POST)
+        user = User.objects.get(username=request.user)
+        account = user.useraccount
         if form.is_valid():
+            score = form.save(commit=False)
+
+            score.account_id=account
+
             round1 = int(form['r1n1'].value()) + int(form['r1n2'].value()) + int(form['r1n3'].value())
-            form.round1Sub = round1
-            form.round1 = round1
+            score.subtotalRoundOne = round1
+            score.cumulativeRoundOne = round1
             round2 = int(form['r2n1'].value()) + int(form['r2n2'].value()) + int(form['r2n3'].value())
-            form.round2Sub = round2
-            form.round2 = round2 + round1
+            score.subtotalRoundTwo = round2
+            score.cumulativeRoundTwo = round2 + round1
             round3 = int(form['r3n1'].value()) + int(form['r3n2'].value()) + int(form['r3n3'].value())
-            form.round3Sub = round3
-            form.round3 = round3 + round2 + round1 
+            score.subtotalRoundThree = round3
+            score.cumulativeRoundThree = round3 + round2 + round1
             round4 = int(form['r4n1'].value()) + int(form['r4n2'].value()) + int(form['r4n3'].value())
-            form.round4Sub = round4
-            form.round4 = round4 + round3 + round2 + round1 
+            score.subtotalRoundFour = round4
+            score.cumulativeRoundFour = round4 + round3 + round2 + round1
             round5 = int(form['r5n1'].value()) + int(form['r5n2'].value()) + int(form['r5n3'].value())
-            form.round5Sub = round5
-            form.round5 = round5 + round4 + round3 + round2 + round1 
+            score.subtotalRoundFive = round5
+            score.cumulativeRoundFive = round5 + round4 + round3 + round2 + round1
             round6 = int(form['r6n1'].value()) + int(form['r6n2'].value()) + int(form['r6n3'].value())
-            form.round6Sub = round6
-            form.round6 = round6 + round5 + round4 + round3 + round2 + round1 
+            score.subtotalRoundSix = round6
+            score.cumulativeRoundSix = round6 + round5 + round4 + round3 + round2 + round1
             round7 = int(form['r7n1'].value()) + int(form['r7n2'].value()) + int(form['r7n3'].value())
-            form.round7Sub = round7
-            form.round7 = round7 + round6 + round5 + round4 + round3 + round2 + round1 
+            score.subtotalRoundSeven = round7
+            score.cumulativeRoundSeven = round7 + round6 + round5 + round4 + round3 + round2 + round1
             round8 = int(form['r8n1'].value()) + int(form['r8n2'].value()) + int(form['r8n3'].value())
-            form.round8Sub = round8
-            form.round8 = round8 + round7 + round6 + round5 + round4 + round3 + round2 + round1 
+            score.subtotalRoundEight = round8
+            score.cumulativeRoundEight = round8 + round7 + round6 + round5 + round4 + round3 + round2 + round1
             round9 = int(form['r9n1'].value()) + int(form['r9n2'].value()) + int(form['r9n3'].value())
-            form.round9Sub = round9
-            form.round9 = round9 + round8 + round7 + round6 + round5 + round4 + round3 + round2 + round1 
+            score.subtotalRoundNine = round9
+            score.cumulativeRoundNine = round9 + round8 + round7 + round6 + round5 + round4 + round3 + round2 + round1
             round10 = int(form['r10n1'].value()) + int(form['r10n2'].value()) + int(form['r10n3'].value())
-            form.round10Sub = round10
-            form.round10 = round10 + round9 + round8 + round7 + round6 + round5 + round4 + round3 + round2 + round1 
-            form.result = round1 + round2 + round3 + round4 + round5 + round6 + round7 + round8 + round9 + round10
+            score.subtotalRoundTen = round10
+            score.cumulativeRoundTen = round10 + round9 + round8 + round7 + round6 + round5 + round4 + round3 + round2 + round1
+            score.score = round1 + round2 + round3 + round4 + round5 + round6 + round7 + round8 + round9 + round10
+            score.save()
             return render(request, 'Scoring/manualScoring.html', {'form': form})
         else:
             return render(request, 'Scoring/manualScoring.html', {'form': form})
     else:
-        form = ManualScoringForm()
+        form = ManualScoringModelForm()
         return render(request, 'Scoring/manualScoring.html', {'form': form})
 
+    return render(request, 'Scoring/manualScoring.html', {'form': form})
 
 
 
