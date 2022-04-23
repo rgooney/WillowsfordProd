c826433cd80fdb3da29232a427634cd125e45c65
David <dwmdominion8@gmail.com>
2022-04-23 14:07:03 -0400

    Added csv export for guests in admin view

--- Willowsford/Registration/admin.py
+++ Willowsford/Registration/admin.py
@@ -81,6 +81,20 @@ class GuestsAdmin(admin.ModelAdmin):
         }),
     )
 
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected as CSV"
 
 admin.site.register(UserAccount, RegistrationAdmin)
 admin.site.register(Guests, GuestsAdmin)
